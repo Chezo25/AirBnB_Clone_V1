6b14ee18c3e2e9341c42f8d632b5acc5427049c2
scrt squirrel_Stod_25 <chezling.stoddard@gmail.com>
2021-05-27 16:58:08 -0300

    Update property.js

--- y/schemas/property.js
+++ y/schemas/property.js
@@ -3,8 +3,58 @@ export default {
     title: 'propertyname',
     type: 'document',
     fields: [{
-        name: 'propertyname',
-        title: 'Title',
-        type: 'string',
-    }]
+            name: 'propertyname',
+            title: 'Title',
+            type: 'string',
+        },
+        {
+            name: 'Location',
+            title: 'Location',
+            type: 'geopoint',
+        },
+        {
+            name: 'PropertyType',
+            title: 'Property Type',
+            type: 'string',
+            options: {
+                list: [{
+                        title: 'House',
+                        value: 'house'
+                    },
+                    {
+                        title: 'Room',
+                        value: 'room'
+                    },
+                    {
+                        title: 'Condo',
+                        value: 'condo'
+                    },
+                    {
+                        title: 'Boutique Hotel',
+                        value: 'boutique hotel'
+                    },
+                ],
+                layout: 'radio'
+            }
+        },
+        {
+            name: 'mainImage',
+            title: 'Main Image',
+            type: 'image',
+            options: {
+                hotspot: true,
+            },
+        },
+        // {
+        //      name: 'image',
+        //      title: 'Image',
+        //      type: 'array',
+        //     of:[{type:'propertyImage'}]
+        //  },
+        {
+            name: 'pricePerNight',
+            title: 'Price Per Night',
+            type: 'number',
+        }
+    ]
 }
